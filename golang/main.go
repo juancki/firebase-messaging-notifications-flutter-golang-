e8c90292f7274fa4780810143281bb7ab9048bb9
juancki <jcgp97@gmail.com>
2020-09-09 21:29:01 +0000

    Golang pushes notifications to flutter

--- golang/main.go
+++ golang/main.go
@@ -20,13 +20,46 @@ import (
 
 var app *firebase.App
 
+func sendToTopic(ctx context.Context, topic string, data map[string]interface{}, notification *messaging.Notification) error {
+    // https://github.com/firebase/firebase-admin-go/blob/cef91acd46f2fc5d0b3408d8154a0005db5bdb0b/snippets/messaging.go#L59
+	// Obtain a messaging.Client from the App.
+    if app == nil {
+        panic("what the fuck")
+    }
+	client, err := app.Messaging(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Messaging client: %v\n", err)
+	}
+    mapString := make(map[string]string)
+
+    for key, value := range  data{
+        strValue := fmt.Sprintf("%v", value)
+        mapString[key] = strValue
+    }
+
+	// See documentation on defining a message payload.
+	message := &messaging.Message{
+		Data: mapString,
+        Notification: notification,
+		Topic: topic,
+	}
+
+	// Send a message to the devices subscribed to the provided topic.
+	response, err := client.Send(ctx, message)
+	if err == nil {
+		fmt.Println(response)
+	}
+	return err
+}
+
+
 func sendToToken(ctx context.Context, registrationToken string, data map[string]interface{}, notification *messaging.Notification) error {
     // https://github.com/firebase/firebase-admin-go/blob/cef91acd46f2fc5d0b3408d8154a0005db5bdb0b/snippets/messaging.go#L27
 	// Obtain a messaging.Client from the App.
     if app == nil {
         panic("what the fuck")
     }
-	client, err := app.Messaging(ctx)
+	client, err := app.Messaging(context.Background())
 	if err != nil {
 		log.Fatalf("error getting Messaging client: %v\n", err)
 	}
@@ -47,7 +80,11 @@ func sendToToken(ctx context.Context, registrationToken string, data map[string]
 	// Send a message to the device corresponding to the provided
 	// registration token.
 	response, err := client.Send(ctx, message)
-	fmt.Println("Successfully sent message:", response)
+	if err == nil{
+		fmt.Println("Successfully sent message:", response)
+	}else {
+		fmt.Println("Error sending message:", err)
+	}
     return err
 	// [END send_to_token_golang]
 }
@@ -84,6 +121,30 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
 }
 
+func SendToTopicHandler(w http.ResponseWriter, r *http.Request) {
+    // Code based on https://firebase.google.com/docs/cloud-messaging/send-message#go
+    var m map[string]interface{}
+    decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&m)
+    if err != nil{
+        panic(err)
+    }
+    var notification *messaging.Notification
+    if n, ok := m["notification"]; ok {
+        nn := n.(map[string]interface{})
+        notification = &messaging.Notification{
+            Title: nn["title"].(string),
+            Body: nn["body"].(string),
+        }
+    }
+    err = sendToTopic(r.Context(), m["topic"].(string), m["data"].(map[string]interface{}), notification)
+    if err != nil{
+        w.WriteHeader(http.StatusNotAcceptable)
+        w.Write([]byte(err.Error()))
+    }else{
+        w.WriteHeader(http.StatusOK)
+	}
+}
 
 func SendToTokenHandler(w http.ResponseWriter, r *http.Request) {
     // Code based on https://firebase.google.com/docs/cloud-messaging/send-message#go
@@ -95,7 +156,7 @@ func SendToTokenHandler(w http.ResponseWriter, r *http.Request) {
     }
     var notification *messaging.Notification
     if n, ok := m["notification"]; ok {
-        nn := n.(map[string]interface{} )
+        nn := n.(map[string]interface{})
         notification = &messaging.Notification{
             Title: nn["title"].(string),
             Body: nn["body"].(string),
@@ -105,8 +166,9 @@ func SendToTokenHandler(w http.ResponseWriter, r *http.Request) {
     if err != nil{
         w.WriteHeader(http.StatusNotAcceptable)
         w.Write([]byte(err.Error()))
-    }
-    w.WriteHeader(http.StatusOK)
+    }else{
+        w.WriteHeader(http.StatusOK)
+	}
 }
 
 func SendToTokensHandler(w http.ResponseWriter, r *http.Request) {
@@ -119,31 +181,55 @@ func SendToTokensHandler(w http.ResponseWriter, r *http.Request) {
     }
     var notification *messaging.Notification
     if n, ok := m["notification"]; ok {
-        nn := n.(map[string]string)
+        nn := n.(map[string]interface{})
         notification = &messaging.Notification{
-            Title: nn["title"],
-            Body: nn["body"],
+            Title: nn["title"].(string),
+            Body: nn["body"].(string),
         }
     }
-    err = sendMulticast(r.Context(), m["tokens"].([]string), m["data"].(map[string]interface{}), notification)
+	mm := m["tokens"].([]interface{})
+	tokens := make([]string, len(mm))
+	for ind, tok := range mm {
+		tokens[ind] = tok.(string)
+	}
+    err = sendMulticast(r.Context(), tokens, m["data"].(map[string]interface{}), notification)
     if err != nil{
         w.WriteHeader(http.StatusNotAcceptable)
         w.Write([]byte(err.Error()))
-    }
-    w.WriteHeader(http.StatusOK)
+    }else{
+        w.WriteHeader(http.StatusOK)
+	}
 }
 func main() {
     // INIT APP
     // https://github.com/firebase/firebase-admin-go/blob/cef91acd46f2fc5d0b3408d8154a0005db5bdb0b/snippets/init.go#L33
-    opt := option.WithCredentialsFile("CREDENTIALS_FILE.json")
-    APP, err := firebase.NewApp(context.Background(), nil, opt)
-    app = APP
+    APP, err := firebase.NewApp(context.Background(), nil)
     if err != nil {
+        fmt.Println("Tyring to use CREDENTIALS_FILE.json.")
+        opt := option.WithCredentialsFile("CREDENTIALS_FILE.json")
+        APP, err = firebase.NewApp(context.Background(), nil, opt)
+        if err != nil {
             log.Fatalf("error initializing app: %v\n", err)
+        }
     }
+    app = APP
+    fmt.Println("USAGE:\n")
+    fmt.Println("\t"+`curl localhost:8080/sendToToken -d '{
+	    "data": {"k": "v"},
+	    "notification": {"title": "t", "body": "b"},
+	    "token":"<YOUR TOKEN>"
+            }'`)
+    fmt.Println()
+    fmt.Println("\t"+`curl localhost:8080/sendToTokens -d '{
+	    "data": {"k": "v"},
+	    "notification": {"title": "t", "body": "b"},
+	    "tokens": ["<TOKEN-0>", "<TOKEN-1>", ...]
+            }'`)
+    fmt.Println()
     r := mux.NewRouter()
     r.HandleFunc("/", HomeHandler)
     r.HandleFunc("/sendToToken", SendToTokenHandler)
+    r.HandleFunc("/sendToTopic", SendToTopicHandler)
     r.HandleFunc("/sendToTokens", SendToTokensHandler)
     port := "8080"
     fmt.Println("Starting Firebase Cloud Messaging Notification Generator server on port:", port)
@@ -156,5 +242,6 @@ func main() {
         ReadTimeout:  15 * time.Second,
     }
     fmt.Printf("%+v\n",srv.Addr)
+    fmt.Println()
     log.Fatal(srv.ListenAndServe())
 }
---
+++ test-go-firebase/go.mod
@@ -0,0 +1,5 @@
+module testing.firebase
+
+go 1.15
+
+require firebase.google.com/go/v4 v4.0.0 // indirect
